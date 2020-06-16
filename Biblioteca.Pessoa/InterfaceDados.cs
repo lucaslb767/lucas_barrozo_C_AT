00b3c6c9bb465454166a3ac25ebc7fab9778e354
Lucas Barrozo <lucaslbarrozo@gmail.com>
2020-06-16 20:40:15 -0300

    Cria classe pessoa

---
+++ Biblioteca.Pessoa/InterfaceDados.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca.Pessoa
+{
+    public interface InterfaceDados
+    {
+        IEnumerable<Pessoa> BuscarPessoas();
+    }
+}
---
+++ Biblioteca.Pessoa/Pessoa.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Biblioteca.Pessoa
+{
+    class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+        public string nome { get; set; }
+        public string sobreNome { get; set; }
+        public DateTime birth { get; set; }
+
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(int id, String Nome, String Sobrenome, DateTime data)
+        {
+            Id = id;
+            nome = Nome;
+            sobreNome = Sobrenome;
+            birth = data;
+        }
+
+        public Pessoa(String Nome, String Sobrenome, DateTime data)
+        {
+            nome = Nome;
+            sobreNome = Sobrenome;
+            birth = data;
+        }
+
+        public int QntosDiasFaltam()
+        {
+            DateTime today = DateTime.Today;
+            DateTime niver = new DateTime(today.Year, birth.Month, birth.Day);
+
+            if (niver < today)
+            {
+                niver = niver.AddYears(1);
+            }
+
+            int diasFaltantes = (niver - today).Days;
+            return diasFaltantes;
+        }
+        public override string ToString()
+        {
+            return " Nome Completo: " + nome + sobreNome +
+                   "\n Data do Aniversario: " + birth.Day + "/" + birth.Month + "/" + birth.Year
+                   + "\n Faltam " + QntosDiasFaltam() + " dias para esse aniversario";
+        }
+    }
+}
